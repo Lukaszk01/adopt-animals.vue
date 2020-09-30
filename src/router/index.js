1876d8bf132217d319ff099acf0704f6abc2a2f9
Lukasz <lukaszkkoziel@gmail.com>
2020-09-30 11:28:57 +0900

    pet info

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
-
 import Cats from '../views/Cats.vue'
 import Dogs from '../views/Dogs.vue'
 import Pet from '../views/Pet.vue'
 
 Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/Cats',
-    name: 'Cats',
-    component: Cats
-  },
-  {
-    path: '/Dogs',
-    name: 'Dogs',
-    component: Dogs
-  },
-  {
-    path: '/Pet/:species/:id',
-    name: 'Pet',
-    component: Pet
-  }
-]
-
-const router = new Router({
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/cats',
+      name: 'cats',
+      component: Cats
+    },
+    {
+      path: '/dogs',
+      name: 'dogs',
+      component: Dogs
+    },
+    {
+      path: '/pets/:species/:id',
+      name: 'pet',
+      component: Pet
+    }
+  ]
 })
-
-export default router
